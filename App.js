8e06b65044dbb846ac46c0f29669f33cfe513514
a2rj <xcz.ardiansyahputra2468@gmail.com>
2020-11-12 14:52:49 +0700

    Get user location

--- App.js
+++ App.js
@@ -1,7 +1,7 @@
-import React, {Component, useState, useEffect} from 'react';
-import { Alert, View, Text, Button} from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { Alert, View, Text, Button } from 'react-native';
 import MapboxGL from '@react-native-mapbox-gl/maps';
-import {lineString as makeLineString} from '@turf/helpers';
+import { lineString as makeLineString } from '@turf/helpers';
 import MapboxDirectionsFactory from '@mapbox/mapbox-sdk/services/directions';
 import Geolocation from '@react-native-community/geolocation';
 
@@ -9,15 +9,14 @@ const accessToken = 'pk.eyJ1IjoiYTJyaiIsImEiOiJja2g3OW11N3MwNmh1MzBsbDQ4NGVrYWNt
 
 MapboxGL.setAccessToken(accessToken);
 
-const directionsClient = MapboxDirectionsFactory({accessToken});
+const directionsClient = MapboxDirectionsFactory({ accessToken });
+const destinationPoint = [117.42784, -8.50641];
 
 export default App = () => {
-  
-  const [startingPoint, setStartingPoint] = useState([])
-  const destinationPoint = [ 117.42784,-8.50641];
-  const [route, setRoute] = useState(null);
 
-  const startDestinationPoints = [startingPoint,  destinationPoint]
+  const [startingPoint, setStartingPoint] = useState([])
+  const [route, setRoute] = useState(null);
+  const startDestinationPoints = [startingPoint, destinationPoint]
 
   useEffect(() => {
     findCoordinates()
@@ -27,94 +26,88 @@ export default App = () => {
     Geolocation.getCurrentPosition(
       position => {
         const location = [position.coords.longitude, position.coords.latitude];
-        // const location = [117.43241,-8.49731];
         setStartingPoint(location)
-        fetchRoute(location)
+        findRoute(location)
       },
       error => Alert.alert(error.message),
       { enableHighAccuracy: true, timeout: 20000, maximumAge: 1000 }
     );
   };
 
-
-  const fetchRoute = async (location) => {
-    console.log(location);
-    // await findCoordinates()
+  const findRoute = async (location) => {
     const reqOptions = {
       waypoints: [
-        {coordinates: location},
-        {coordinates: destinationPoint},
+        { coordinates: location },
+        { coordinates: destinationPoint },
       ],
       profile: 'driving-traffic',
       geometries: 'geojson',
     };
 
     const res = await directionsClient.getDirections(reqOptions).send();
-
     const newRoute = makeLineString(res.body.routes[0].geometry.coordinates);
-    // console.log(newRoute);
     setRoute(newRoute);
-  };
+  }
 
   const renderAnnotations = () => {
     return (
       startDestinationPoints.map((point, index) => (
         <MapboxGL.PointAnnotation
-            key={`${index}-PointAnnotation`}
-            id={`${index}-PointAnnotation`}
-            coordinate={point}> 
-            <View style={{
-              height: 30, 
-              width: 30, 
-              backgroundColor: '#00cccc', 
-              borderRadius: 50, 
-              borderColor: '#fff', 
-              borderWidth: 3
-            }} 
+          key={`${index}-PointAnnotation`}
+          id={`${index}-PointAnnotation`}
+          coordinate={point}>
+          <View style={{
+            height: 30,
+            width: 30,
+            backgroundColor: '#00cccc',
+            borderRadius: 50,
+            borderColor: '#fff',
+            borderWidth: 3
+          }}
           />
         </MapboxGL.PointAnnotation>
       ))
     );
   }
 
-
   return startingPoint.length > 0 ? (
-    <View style={{flex: 1, height: "100%", width: "100%" }}>
+    <View style={{ flex: 1, height: "100%", width: "100%" }}>
       <MapboxGL.MapView
         styleURL={MapboxGL.StyleURL.Street}
         zoomLevel={11}
-        zoomLevel={true}
+        zoomEnabled={true}
         centerCoordinate={startingPoint}
-        style={{flex: 1}}>
-          <MapboxGL.Camera
-            zoomLevel={11}
-            centerCoordinate={startingPoint}
-            animationMode={'flyTo'}
-            animationDuration={0}
-            >
-          </MapboxGL.Camera>
-          <MapboxGL.UserLocation
-            visible={true}
-            showsUserHeadingIndicator={false}
-          />
-          {renderAnnotations()}
-          {
-            route && (
-              <MapboxGL.ShapeSource id='shapeSource' shape={route}>
-              <MapboxGL.LineLayer id='lineLayer' style={{lineWidth: 5, lineJoin: 'bevel', lineColor: '#ff0000'}} />
+        style={{ flex: 1 }}>
+        <MapboxGL.Camera
+          zoomLevel={11}
+          centerCoordinate={startingPoint}
+          animationMode={'flyTo'}
+          animationDuration={0}
+        >
+        </MapboxGL.Camera>
+        {/* Buat saat user berpindah maka route juga berubah dgn menerapkan onchange maka set startingPoint baru */}
+        {/* <MapboxGL.UserLocation
+          visible={true}
+          showsUserHeadingIndicator={false}
+        /> */}
+        {renderAnnotations()}
+        {
+          route && (
+            <MapboxGL.ShapeSource id='shapeSource' shape={route}>
+              <MapboxGL.LineLayer id='lineLayer' style={{ lineWidth: 5, lineJoin: 'bevel', lineColor: '#ff0000' }} />
             </MapboxGL.ShapeSource>
           )
         }
       </MapboxGL.MapView>
     </View>
-  ) : 
-  <View>
-    <Text>Lokasi tidak ditemukan { startingPoint }</Text>
-    <Button
-      onPress={findCoordinates}
-      title="Learn More"
-      color="#841584"
-      accessibilityLabel="Learn more about this purple button"
-    />
-  </View>
+  ) :
+    <View>
+      <Text>Lokasi tidak ditemukan {startingPoint}</Text>
+      <Button
+        onPress={findCoordinates}
+        title="Learn More"
+        color="#841584"
+        accessibilityLabel="Learn more about this purple button"
+      />
+    </View>
 }
