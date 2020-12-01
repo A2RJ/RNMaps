ab377f6e5dd3bbf8e10fdd101cba9e61864dd32b
a2rj <xcz.ardiansyahputra2468@gmail.com>
2020-12-02 01:11:11 +0700

    Add more efect

--- App.js
+++ App.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import { Alert, View, Text, Button } from 'react-native';
 import MapboxGL from '@react-native-mapbox-gl/maps';
 import { lineString as makeLineString } from '@turf/helpers';
@@ -14,7 +14,7 @@ const directionsClient = MapboxDirectionsFactory({ accessToken });
 const destinationPoint = [117.42784, -8.50641];
 
 export default App = () => {
-
+  const mapRef = useRef(null);
   const [startingPoint, setStartingPoint] = useState([])
   const [route, setRoute] = useState(null);
   const startDestinationPoints = [startingPoint, destinationPoint]
@@ -50,6 +50,10 @@ export default App = () => {
     setRoute(newRoute);
   }
 
+  const onRegionDidChange = async () => {
+    this.mapRef.camera.moveTo(startingPoint, 200)
+  }
+
   const renderAnnotations = () => {
     return (
       startDestinationPoints.map((point, index) => (
@@ -65,55 +69,52 @@ export default App = () => {
             borderColor: '#fff',
             borderWidth: 3
           }}
-          />
+          ></View>
         </MapboxGL.PointAnnotation>
       ))
     );
   }
 
-  return (
-    <View style={{ flex: 1, height: "100%", width: "100%" }}>
-      { startingPoint.length > 0 ?
-        <View>
-          <MapboxGL.MapView
-          styleURL={MapboxGL.StyleURL.Street}
+  return startingPoint.length > 0 ? (
+    <View style={{ flex: 1 }}>
+      <MapboxGL.MapView
+        styleURL={MapboxGL.StyleURL.Street}
+        zoomLevel={15}
+        zoomEnabled={true}
+        centerCoordinate={startingPoint}
+        rotateEnabled={true}
+        logoEnabled={false}
+        compassEnabled={true}
+        style={{ flex: 1 }}>
+        <MapboxGL.Camera
           zoomLevel={15}
-          zoomEnabled={true}
           centerCoordinate={startingPoint}
-          rotateEnabled={true}
-          logoEnabled={false}
-          compassEnabled={true}
-          style={{ flex: 1 }}>
-
-          <MapboxGL.Camera
-            zoomLevel={15}
-            centerCoordinate={startingPoint}
-            animationMode={'flyTo'}
-            animationDuration={2000}
-            followUserLocation={true}
-            followUserMode="compass"
-            followZoomLevel={15} />
-          {/* Untuk menampilkan icon driver, agar dapat tracking maka connect socket.io kemudian coordinates listen */}
-          <MapboxGL.MarkerView coordinate={startingPoint}>
-            <FontAwesomeIcon icon={faCoffee} />
-          </MapboxGL.MarkerView>
-
-          {renderAnnotations()}
-          { route && (
-              <MapboxGL.ShapeSource id='shapeSource' shape={route}>
-                <MapboxGL.LineLayer id='lineLayer' style={{ lineWidth: 5, lineJoin: 'bevel', lineColor: '#ff0000' }} />
-              </MapboxGL.ShapeSource>
-            )}
-          </MapboxGL.MapView>
-
-        </View>
-
-        :
-        <View>
-          <Button onPress={() => findCoordinates()} title="Pick location" />
-        </View>
-      }
-{/* <Button onPress={ () => console.log('button') } title="Maps show" />        */}
+          animationMode={'flyTo'}
+          animationDuration={2000}
+          followUserLocation={true}
+          followUserMode="compass"
+          followZoomLevel={15} />
+        {/* Untuk menampilkan icon driver, agar dapat tracking maka connect socket.io kemudian coordinates listen */}
+        <MapboxGL.MarkerView coordinate={startingPoint}>
+          <FontAwesomeIcon icon={faCoffee} />
+        </MapboxGL.MarkerView>
+        
+        {renderAnnotations()}
+        {route && (
+          <MapboxGL.ShapeSource id='shapeSource' shape={route}>
+            <MapboxGL.LineLayer id='lineLayer' style={{ lineWidth: 5, lineJoin: 'bevel', lineColor: '#ff0000' }} />
+          </MapboxGL.ShapeSource>
+        )}
+      </MapboxGL.MapView>
+      <Button onPress={ () => onRegionDidChange()}
+      title="center" style={{
+            position: 'absolute',
+            bottom: 0,
+            width: '100%'
+          }} />
     </View>
-  )
+  ) :
+    <View>
+      <Button onPress={() => findCoordinates()} title="Pick location" />
+    </View>
 }
